c73dee5e5fae53f5620b88923517e21e4a26f905
tarzanbappa <janith505@gmail.com>
2016-04-01 10:23:46 +0530

    Added Bootstrap tree

--- Welkin.UI/Scripts/app/handlers/DraftHandler.js
+++ Welkin.UI/Scripts/app/handlers/DraftHandler.js
@@ -17,42 +17,7 @@
         },
         initControlls:function () {
             
-              var inlineDefault = new kendo.data.HierarchicalDataSource({
-            data: [
-                {
-                    text: "Case", items: [
-                      {
-                          text: "Land", items: [
-                              {
-                                  text:"Mortgage.docx"
-                              }
-                          ]
-                      },
-                      { text: "Partition" }
-                    ]
-                },
-                {
-                    text: "Deed"
-                },
-                {
-                    text: "General"
-                },
-                {
-                    text: "Misc", items: [
-                        {
-                            text: "Pedigree Diagram V1"
-                        },
-                        {
-                            text: "Pedigree Diagram V2"
-                        }
-                    ]
-                }
-            ]
-        });
-
-        $("#treeview-left").kendoTreeView({
-            dataSource: inlineDefault
-        });
+          
         },
         masterDataResponse: function(a) {
             var grantees = JSON.parse(JSON.parse(a).JsonResult)[0].Grantees;
